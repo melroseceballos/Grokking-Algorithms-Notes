229189db85a1e1bcfa5fb7398870750a758b822c
Melrose Ceballos <120954128+melroseceballos@users.noreply.github.com>
2023-05-21 11:10:33 -0700

    Update BinarySearch.js

--- BinarySearch.js
+++ BinarySearch.js
@@ -5,3 +5,27 @@
 - cuts a list in half and determines which half to explore
 depending on what the user needs
 **/
+
+// Exercise 1: pass had to redo 2x
+// had issues with syntax 
+// fixed it and it worked
+
+// REMEMBER TO TRIPLE CHECK EVERYTHING
+// take time when checking code
+BinarySearch = (arr,pick) => {
+  let low = 0
+  let high = arr.length - 1
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2)
+ if(arr[mid] === pick){
+   return mid
+ }else if(arr[mid] <= pick){
+		low = mid + 1
+}else if (arr[mid] >= pick ){
+		high = mid - 1
+}
+    }
+  return false
+}
+
+console.log(BinarySearch([1,3,4,5], 3))
