1ece942b668ff0748dce12fc418eb76c6933ffbe
Melrose Ceballos <120954128+melroseceballos@users.noreply.github.com>
2023-05-22 09:38:07 -0700

    Update SelectionSort.js

--- SelectionSort.js
+++ SelectionSort.js
@@ -1,5 +1,2 @@
 /** Selection Sort **/
 
-/** Linked List**/
-// data can be stored anywhere as they point to each other for location purposes
-// any current list will contain the address for the next list and so on so forth
