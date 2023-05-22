cae7d4abc5c9e58fb9091b272fe5ce1fed058982
Melrose Ceballos <120954128+melroseceballos@users.noreply.github.com>
2023-05-22 12:26:33 -0700

    Update Recursion.js

--- Recursion.js
+++ Recursion.js
@@ -7,3 +7,10 @@ the recursion **/
 /** Recursive case - it is when the function hasnt
 met the base case so the recursive case keeps calling itself/recursion
 until base case is met **/
+
+
+/** all functions go into a call stack 
+which is like a priority queue. a stack can push or pop a function
+if the function is too large. it takes up too much memory 
+because the call stack takes up memory. A way to prevent this
+is by using a tail recursion**/
