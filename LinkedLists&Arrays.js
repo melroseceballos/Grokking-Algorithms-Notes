a850640ca114af93c354b8a1bf417ee1bb87d439
Melrose Ceballos <120954128+melroseceballos@users.noreply.github.com>
2023-05-22 09:39:23 -0700

    Update LinkedLists&Arrays.js

--- LinkedLists&Arrays.js
+++ LinkedLists&Arrays.js
@@ -2,7 +2,12 @@
 
 /** Linked lists are good for deletions, 
 insertions, and memory saving. The downside
-of it is it only allows sequential accesss*/
+of it is it only allows sequential accesss
+The way linkedlists works is the current data 
+has the address for the next data so on and so forth
+*/
+
+
 
 /** Arrays are sub to Linked list but
 the upside to it is it allows random
