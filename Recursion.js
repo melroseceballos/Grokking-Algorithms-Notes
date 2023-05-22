e4b865b3d566c9e2d5abca5193e7c11b1dfa837c
Melrose Ceballos <120954128+melroseceballos@users.noreply.github.com>
2023-05-22 10:59:18 -0700

    Update Recursion.js

--- Recursion.js
+++ Recursion.js
@@ -4,4 +4,6 @@
 function has to meet in order to no longer continue
 the recursion **/
 
-/** Recursion case
+/** Recursive case - it is when the function hasnt
+met the base case so the recursive case keeps calling itself/recursion
+until base case is met **/
