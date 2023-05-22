c6ecc4fa9d77588c461d493717d5bf6e87358f52
Melrose Ceballos <120954128+melroseceballos@users.noreply.github.com>
2023-05-22 09:33:07 -0700

    Update LinkedLists&Arrays.js

--- LinkedLists&Arrays.js
+++ LinkedLists&Arrays.js
@@ -4,3 +4,6 @@
 insertions, and memory saving. The downside
 of it is it only allows sequential accesss*/
 
+/** Arrays are sub to Linked list but
+the upside to it is it allows random
+access so it's faster to read through**/
