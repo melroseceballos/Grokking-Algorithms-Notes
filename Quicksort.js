f15bf5360d484a61fe377b929b6eba7ba68c9c53
Melrose Ceballos <120954128+melroseceballos@users.noreply.github.com>
2023-05-24 14:22:51 -0700

    Update Quicksort.js

--- Quicksort.js
+++ Quicksort.js
@@ -10,3 +10,19 @@ than the pivot
 - combine and done
 **/
 
+const quickSort = (arr) => {
+  if (arr.length < 2) return arr; // Base case: an array with 0 or 1 element is already sorted
+  const pivot = arr[0]; // Choosing the first element as the pivot
+  const less = [];
+  const greater = [];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] <= pivot) {
+      less.push(arr[i]);
+    } else {
+      greater.push(arr[i]);
+    }
+  }
+  const sortedLess = quickSort(less); // Recursively sort the elements less than the pivot
+  const sortedGreater = quickSort(greater); // Recursively sort the elements greater than the pivot
+  return [...sortedLess, pivot, ...sortedGreater]; // Concatenate the sorted sub-arrays with the pivot in between
+};
