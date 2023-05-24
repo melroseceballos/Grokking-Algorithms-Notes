33035a31081cbaf49f062378a59f2e66e9a966e3
Melrose Ceballos <120954128+melroseceballos@users.noreply.github.com>
2023-05-24 12:26:46 -0700

    Update Quicksort.js

--- Quicksort.js
+++ Quicksort.js
@@ -1,5 +1,12 @@
 /** QUICKSORT 
 faster than selection sort and is more commonly used 
 
+-
+PSEUDOCODE
+- pick a pivot point
+- divide the array into subarrays depending if they are smaller or bigger 
+than the pivot
+- call quicksort recursively on both subarrays
+- combine and done
 **/
 
