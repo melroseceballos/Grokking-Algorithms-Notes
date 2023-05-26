a8bbcddb28ece4c073dca29c1d6d9b5e3c961b61
Melrose Ceballos <120954128+melroseceballos@users.noreply.github.com>
2023-05-26 12:01:07 -0700

    Update HashTables.js

--- HashTables.js
+++ HashTables.js
@@ -1,4 +1,6 @@
 /**
 - Takes a string and stores the value in a list
-- it then returns the index of which the value of such string is stored 
+- it then returns the index of which the value of such string is stored
+- The index must be constant or else your hash function is not working
+- it must not also give the same number all the time or else you get collisions
 */
